d896824b0cc38f2d773d1df1613bf05244ae8b78
Tanguy Krotoff <tkrotoff@gmail.com>
2018-12-13 01:32:18 +0100

    React.SFC => React.FunctionComponent

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const Layout: React.SFC = props => <div className="container-fluid">{props.children}</div>;
+const Layout: React.FunctionComponent = props => <div className="container-fluid">{props.children}</div>;
 
 export default Layout;
--- src/PageNotFound.tsx
+++ src/PageNotFound.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 
-const PageNotFound: React.SFC<RouteComponentProps<{}>> = ({ location }) => (
+const PageNotFound: React.FunctionComponent<RouteComponentProps<{}>> = ({ location }) => (
   <>
     <h1>Whoops</h1>
     <p>
