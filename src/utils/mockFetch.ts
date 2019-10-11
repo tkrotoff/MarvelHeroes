04782930b7c065b465d75e93740a9c63647de77e
Tanguy Krotoff <tkrotoff@gmail.com>
2019-10-12 01:10:27 +0200

    Simplify mockFetch

--- src/utils/mockFetch.ts
+++ src/utils/mockFetch.ts
@@ -1,9 +1,7 @@
-// See [How to test api calls using fetch?](https://github.com/facebook/create-react-app/issues/967)
+// See [How to test api calls using fetch?](https://github.com/facebook/create-react-app/issues/967#issuecomment-396771497)
 export function mockFetch(data: object) {
-  return jest.fn().mockImplementation(() =>
-    Promise.resolve({
-      ok: true,
-      json: () => data
-    })
-  );
+  return jest.fn().mockResolvedValueOnce({
+    ok: true,
+    json: () => Promise.resolve(data)
+  } as Response);
 }
