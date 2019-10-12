adea3a3c002c2bf7590e845b227024cd0cb9e5e3
Tanguy Krotoff <tkrotoff@gmail.com>
2019-10-12 02:13:57 +0200

    Refactoring

--- src/api/Marvel.ts
+++ src/api/Marvel.ts
@@ -4,34 +4,6 @@ const API_PUBLIC = '298bab46381a6daaaee19aa5c8cafea5';
 const API_PRIVATE = 'b0223681fced28de0fe97e6b9cd091dd36a5b71d';
 const BASE_URL = 'https://gateway.marvel.com';
 
-export function getQueryParams(offset?: number) {
-  const ts = Date.now();
-
-  const params = {
-    ts,
-    apikey: API_PUBLIC,
-    hash: md5(ts + API_PRIVATE + API_PUBLIC),
-    limit: 50,
-    offset
-  };
-
-  // See [How to pass url query params?](https://github.com/github/fetch/issues/256)
-  return Object.keys(params)
-    .filter(key => (params as any)[key] !== undefined)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent((params as any)[key])}`)
-    .join('&');
-}
-
-export type Characters = Character[];
-
-export async function fetchCharacters(offset: number) {
-  const response = await fetch(`${BASE_URL}/v1/public/characters?${getQueryParams(offset)}`);
-  // See [Handling Failed HTTP Responses With fetch()](https://www.tjvantoll.com/2015/09/13/fetch-and-errors/)
-  if (!response.ok) throw new Error(response.statusText);
-  const data = await response.json();
-  return data.data.results as Characters;
-}
-
 export interface CharacterCategory {
   available: number;
   collectionURI: string;
@@ -67,6 +39,34 @@ export interface Character {
   ];
 
   [index: string]: CharacterCategory | string | number | object;
+}
+
+export type Characters = Character[];
+
+export function getQueryParams(offset?: number) {
+  const ts = Date.now();
+
+  const params = {
+    ts,
+    apikey: API_PUBLIC,
+    hash: md5(ts + API_PRIVATE + API_PUBLIC),
+    limit: 50,
+    offset
+  };
+
+  // See [How to pass url query params?](https://github.com/github/fetch/issues/256)
+  return Object.keys(params)
+    .filter(key => (params as any)[key] !== undefined)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent((params as any)[key])}`)
+    .join('&');
+}
+
+export async function fetchCharacters(offset: number) {
+  const response = await fetch(`${BASE_URL}/v1/public/characters?${getQueryParams(offset)}`);
+  // See [Handling Failed HTTP Responses With fetch()](https://www.tjvantoll.com/2015/09/13/fetch-and-errors/)
+  if (!response.ok) throw new Error(response.statusText);
+  const data = await response.json();
+  return data.data.results as Characters;
 }
 
 export async function fetchCharacter(id: string) {
