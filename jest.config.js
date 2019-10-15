ad3753462824d7f16801aea28f282e33b7aeb079
Tanguy Krotoff <tkrotoff@gmail.com>
2019-10-15 11:21:52 +0200

    Stricter testRegex

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,10 @@ const { defaults } = require('jest-config');
 
 module.exports = {
   setupFiles: ['./jest.setup.ts'],
-  coveragePathIgnorePatterns: [...defaults.coveragePathIgnorePatterns, './jest.setup.ts']
+  coveragePathIgnorePatterns: [...defaults.coveragePathIgnorePatterns, './jest.setup.ts'],
+
+  // By default Jest allows for __tests__/*.js, *.spec.js and *.test.js
+  // See https://jestjs.io/docs/en/configuration#testregex-string-array-string
+  // let's be strict and use *.test.js only
+  testRegex: '\\.test\\.tsx?$'
 };
