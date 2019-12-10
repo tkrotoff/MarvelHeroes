eb4befe1ddf008d87002a99062c577d970d42c13
Tanguy Krotoff <tkrotoff@gmail.com>
2019-12-10 07:53:38 +0100

    react-hooks/exhaustive-deps warn => error

--- .eslintrc.js
+++ .eslintrc.js
@@ -67,7 +67,7 @@ module.exports = {
     'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
 
     'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn'
+    'react-hooks/exhaustive-deps': 'error'
   },
 
   overrides: [
