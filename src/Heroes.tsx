f0c922fd92d67002ceeb963e288169b3d60c3a4d
Tanguy Krotoff <tkrotoff@gmail.com>
2020-01-02 00:09:12 +0100

    Improve heroes cards margins + on mobile

--- src/Heroes.tsx
+++ src/Heroes.tsx
@@ -4,29 +4,33 @@ import { Link } from 'react-router-dom';
 import * as Marvel from './api/Marvel';
 
 function renderHeroes(characters: Marvel.Characters) {
-  return characters.map(character => (
-    <section key={character.id} className="card m-3" style={{ width: '200px' }}>
-      <img
-        src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
-        alt={character.name}
-        className="card-img-top"
-        style={{
-          // Fix for IE11
-          // See [Flexbox on IE11: image stretched for no reason?](https://stackoverflow.com/q/36822370)
-          flexShrink: 0,
+  return (
+    <div className="card-deck mt-3">
+      {characters.map(character => (
+        <section key={character.id} className="hero card">
+          <img
+            src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+            alt={character.name}
+            className="card-img-top"
+            style={{
+              // Fix for IE11
+              // See [Flexbox on IE11: image stretched for no reason?](https://stackoverflow.com/q/36822370)
+              flexShrink: 0,
 
-          // Fix for IE10
-          height: 'auto'
-        }}
-      />
-      <div className="card-body">
-        <h5 className="card-title">{character.name}</h5>
-      </div>
-      <div className="card-footer border-top-0">
-        <Link to={`/heroes/${character.id}`}>Details</Link>
-      </div>
-    </section>
-  ));
+              // Fix for IE10
+              height: 'auto'
+            }}
+          />
+          <div className="card-body">
+            <h5 className="card-title">{character.name}</h5>
+          </div>
+          <div className="card-footer border-top-0">
+            <Link to={`/heroes/${character.id}`}>Details</Link>
+          </div>
+        </section>
+      ))}
+    </div>
+  );
 }
 
 interface Props {
@@ -50,9 +54,11 @@ export function Heroes(props: Props) {
     fetch(page);
   }, [page]);
 
-  return characters !== undefined ? (
-    <div className="d-flex flex-wrap">{renderHeroes(characters)}</div>
-  ) : (
-    <p>Please wait...</p>
-  );
+  if (characters === undefined) {
+    return <p>Please wait...</p>;
+  }
+  if (characters.length === 0) {
+    return <p>No results found :(</p>;
+  }
+  return renderHeroes(characters);
 }
