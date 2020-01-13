4f5df3a84c962e9166eb58a8b754a226bf16cd2d
Tanguy Krotoff <tkrotoff@gmail.com>
2020-01-13 23:17:41 +0100

    Coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -10,5 +10,14 @@ module.exports = {
   // By default Jest allows for __tests__/*.js, *.spec.js and *.test.js
   // See https://jestjs.io/docs/en/configuration#testregex-string-array-string
   // let's be strict and use *.test.js only
-  testRegex: '\\.test\\.tsx?$'
+  testRegex: '\\.test\\.tsx?$',
+
+  coverageThreshold: {
+    global: {
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100
+    }
+  }
 };
