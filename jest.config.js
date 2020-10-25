0b6f7b1fa3987c49de74a5c6158eb3b810824d1c
Tanguy Krotoff <tkrotoff@gmail.com>
2020-10-25 22:35:51 +0100

    No need to specify coveragePathIgnorePatterns

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,8 @@
 // @ts-check
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { defaults } = require('jest-config');
-
 /** @type Partial<import('@jest/types').Config.InitialOptions> */
 const config = {
   setupFiles: ['./jest.setup.ts'],
-  coveragePathIgnorePatterns: [...defaults.coveragePathIgnorePatterns, './jest.setup.ts'],
 
   // By default Jest allows for __tests__/*.js, *.spec.js and *.test.js
   // https://jestjs.io/docs/en/configuration#testregex-string-array-string
