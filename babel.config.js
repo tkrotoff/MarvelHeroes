ab9832c8af7adb60eb83d7943f4d63df238a638a
Tanguy Krotoff <tkrotoff@gmail.com>
2020-10-25 22:56:13 +0100

    No need for some Babel plugins anymore

--- babel.config.js
+++ babel.config.js
@@ -5,7 +5,7 @@ module.exports = {
     [
       '@babel/preset-env',
       {
-        // Cannot use 'usage' otherwise IE10 fails with "'Set' is undefined"
+        // Cannot use 'usage' otherwise IE 10 fails with "'Set' is undefined"
         useBuiltIns: 'entry',
 
         // https://babeljs.io/blog/2019/03/19/7.4.0#core-js-3-7646-https-githubcom-babel-babel-pull-7646
@@ -17,9 +17,5 @@ module.exports = {
     '@babel/preset-react',
     '@babel/preset-typescript'
   ],
-  plugins: [
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-object-rest-spread',
-    '@babel/plugin-proposal-nullish-coalescing-operator'
-  ]
+  plugins: ['@babel/plugin-proposal-class-properties']
 };
